b707bfe9c6f5c3e1cfefba8e561b1f3cc2f40931
felipe_e_lima <felipeeduardo.eg@gmail.com>
2023-03-22 09:18:17 -0300

    tela

--- app/src/main/java/com/example/calculadorabug/MainActivity.java
+++ app/src/main/java/com/example/calculadorabug/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.calculadorabug;
 
-import androidx.appcompat.app.AppCompatActivity;
+import java.util.Scanner;
 
-import android.os.Bundle;
+public class Calculadoraug {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double num1, num2, resultado;
+        char operacao;
 
-public class MainActivity extends AppCompatActivity {
+        System.out.print("Digite o primeiro número: ");
+        num1 = scanner.nextDouble();
+        System.out.print("Digite a operação (+, -, , /): ");
+        operacao = scanner.next().charAt(0);
+        System.out.print("Digite o segundo número: ");
+        num2 = scanner.nextDouble();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        switch (operacao) {
+            case '+':
+                resultado = num1 + num2;
+                break;
+            case '-':
+                resultado = num1 - num2;
+                break;
+            case '':
+                resultado = num1 * num2;
+                break;
+            case '/':
+                resultado = num1 / num2;
+                break;
+            default:
+                System.out.println("Operação inválida!");
+                return;
+        }
+
+        System.out.println(num1 + " " + operacao + " " + num2 + " = " + resultado);
     }
 }
